98ce4149c16aebd29ac2d4ea9b088500879f7fe6
Ryan Bourgeois <bluedragonx@gmail.com>
2021-10-26 18:53:23 -0700

    Less debug, more macros.

--- head/debug.h
+++ head/debug.h
@@ -5,54 +5,72 @@
 #include <stdint.h>
 
 
-#ifdef DEBUG_STREAM
-extern Stream* Debug = &DEBUG_STREAM;
+#ifdef DEBUG_SERIAL
 
-size_t printDebugFrame(uint32_t id, uint8_t len, uint8_t* data);
+size_t printDebugFrame(uint32_t id, uint8_t len, uint8_t* data) {
+    size_t n = 0;
+    n += DEBUG_SERIAL.print(id, HEX);
+    n += DEBUG_SERIAL.print("#");
+    for (int i = 0; i < len; i++) {
+        if (data[i] <= 0x0F) {
+            n += DEBUG_SERIAL.print("0");
+        }
+        n += DEBUG_SERIAL.print(data[i], HEX);
+        if (i < len-1) {
+            n += DEBUG_SERIAL.print(":");
+        }
+    }
+    return n;
+}
+
+#define DEBUG_BEGIN(BAUD) DEBUG_SERIAL.begin(BAUD)
 
 #define INFO_MSG(MSG) ({\
-    Debug->print("[INFO]  ");\
-    Debug->println(MSG);\
+    DEBUG_SERIAL.print("[INFO]  ");\
+    DEBUG_SERIAL.println(MSG);\
 })
 #define INFO_MSG_VAL(MSG, VAL) ({\
-    Debug->print("[INFO]  ");\
-    Debug->print(MSG);\
-    Debug->println(VAL);\
+    DEBUG_SERIAL.print("[INFO]  ");\
+    DEBUG_SERIAL.print(MSG);\
+    DEBUG_SERIAL.println(VAL);\
 })
 #define INFO_MSG_VAL_FMT(MSG, VAL, FMT) ({\
-    Debug->print("[INFO]  ");\
-    Debug->print(MSG);\
-    Debug->println(VAL, FMT);\
+    DEBUG_SERIAL.print("[INFO]  ");\
+    DEBUG_SERIAL.print(MSG);\
+    DEBUG_SERIAL.println(VAL, FMT);\
 })
 #define INFO_MSG_FRAME(MSG, ID, LEN, DATA) ({\
-    Debug->print("[INFO]  ");\
-    Debug->print(MSG);\
-    Debug->printDebugFrame(ID, LEN, Data);\
-    Debug->println("");\
+    DEBUG_SERIAL.print("[INFO]  ");\
+    DEBUG_SERIAL.print(MSG);\
+    printDebugFrame(ID, LEN, DATA);\
+    DEBUG_SERIAL.println("");\
 })
 
 #define ERROR_MSG(MSG) ({\
-    Debug->print("[ERROR] ");\
-    Debug->println(MSG);\
+    DEBUG_SERIAL.print("[ERROR] ");\
+    DEBUG_SERIAL.println(MSG);\
 })
 #define ERROR_MSG_VAL(MSG, VAL) ({\
-    Debug->print("[ERROR] ");\
-    Debug->print(MSG);\
-    Debug->println(VAL);\
+    DEBUG_SERIAL.print("[ERROR] ");\
+    DEBUG_SERIAL.print(MSG);\
+    DEBUG_SERIAL.println(VAL);\
 })
 #define ERROR_MSG_VAL_FMT(MSG, VAL, FMT) ({\
-    Debug->print("[ERROR] ");\
-    Debug->print(MSG);\
-    Debug->println(VAL, FMT);\
+    DEBUG_SERIAL.print("[ERROR] ");\
+    DEBUG_SERIAL.print(MSG);\
+    DEBUG_SERIAL.println(VAL, FMT);\
 })
 #define ERROR_MSG_FRAME(MSG, ID, LEN, DATA) ({\
-    Debug->print("[ERROR] ");\
-    Debug->print(MSG);\
-    Debug->printDebugFrame(ID, LEN, Data);\
-    Debug->println("");\
+    DEBUG_SERIAL.print("[ERROR] ");\
+    DEBUG_SERIAL.print(MSG);\
+    printDebugFrame(ID, LEN, DATA);\
+    DEBUG_SERIAL.println("");\
 })
 
 #else
+
+#define DEBUG_BEGIN(BAUD)
+#define DEBUG_BEGIN(BAUD, CONFIG)
 
 #define INFO_MSG(MSG)
 #define INFO_MSG_VAL(MSG, VAL)
