10f4c87ad7c04952246c3690ffed9ad022282903
Ryan Bourgeois <bluedragonx@gmail.com>
2021-10-31 00:28:02 -0700

    Add more binary helpers.

--- climate/binary.cpp
+++ climate/binary.cpp
@@ -6,12 +6,24 @@ bool getBit(byte* b, uint8_t offset, uint8_t bit) {
 
 void setBit(byte* b, uint8_t offset, uint8_t bit, bool value) {
     if (value) {
-        b[offset] |= 1 << bit;
+        b[offset] |= (1 << bit);
     } else {
         b[offset] &= ~(1 << bit);
     }
 }
 
+bool xorBits(byte* b1, byte* b2, uint8_t offset, uint8_t bit) {
+    return (b1[offset] >> bit & 1) != (b2[offset] >> bit & 1);
+}
+
+bool setBitXor(byte* b, uint8_t offset, uint8_t bit, bool value) {
+  if ((b[offset] >> bit ^ (byte)value) != 0) {
+    b[offset] ^= (1 << bit);
+    return true;
+  }
+  return false;
+}
+
 void toggleBit(byte* b, uint8_t offset, uint8_t bit) {
-    b[offset] ^= 1 << bit;
+    b[offset] ^= (1 << bit);
 }
--- climate/binary.h
+++ climate/binary.h
@@ -9,7 +9,15 @@ bool getBit(byte* b, uint8_t offset, uint8_t bit);
 // Set a bit in a byte array.
 void setBit(byte* b, uint8_t offset, uint8_t bit, bool value);
 
+// Check if the same bit in the two bytes differs.
+bool xorBits(byte* b1, byte* b2, uint8_t offset, uint8_t bit);
+
+// Set a bit if it differs from the current value. Return true if the bit was
+// set or false if it was not.
+bool setBitXor(byte* b, uint8_t offset, uint8_t bit, bool value);
+
 // Toggle a bit in a byte array.
 void toggleBit(byte* b, uint8_t offset, uint8_t bit);
+
 
 #endif  // __R51_BINARY_H__
