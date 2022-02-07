9edb23637cecc9f6f3b20d3713e703c9823074ef
Ryan Bourgeois <bluedragonx@gmail.com>
2022-02-06 22:21:28 -0800

    Add mockable clock and gpio implementation.

---
+++ controller/src/clock.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+#include "clock.h"
+
+
+// Unshasow Arduino functions.
+inline uint32_t arduino_millis() { return millis(); }
+inline void arduino_delay(uint32_t ms) { delay(ms); }
+
+class RealClock : public Clock {
+    public:
+        uint32_t millis() override {
+            return arduino_millis();
+        }
+
+        void delay(uint32_t ms) override {
+            arduino_delay(ms);
+        }
+};
+
+RealClock real_clock;
+
+Clock* Clock::real() {
+    return &real_clock;
+}
---
+++ controller/src/clock.h
@@ -0,0 +1,20 @@
+#ifndef __R51_CLOCK__
+#define __R51_CLOCK__
+
+// Base clock interface. Allows the clock to be mocked.
+class Clock {
+    public:
+        // Return a real clock.
+        static Clock* real();
+
+        Clock() = default;
+        virtual ~Clock() = default;
+
+        // Return the number of milliseconds since the Arduino started.
+        virtual uint32_t millis() = 0;
+
+        // Pause the Arduino for the given number of milliseconds.
+        virtual void delay(uint32_t) = 0;
+};
+
+#endif  // __R51_CLOCK__
