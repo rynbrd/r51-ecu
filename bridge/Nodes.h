5b81750574156c7603120479a768c41894e3c9ac
Ryan Bourgeois <bluedragonx@gmail.com>
2022-08-16 17:27:49 -0700

    No using statements in headers.

--- bridge/Nodes.h
+++ bridge/Nodes.h
@@ -7,34 +7,31 @@
 
 #include "Debug.h"
 
-using ::Canny::Frame;
-using ::Canny::Error;
-
 // CAN node which filters relevant R51 vehicle frames.
 class FilteredCAN : public R51::CANNode {
     public:
         FilteredCAN() : R51::CANNode(&CAN) {}
 
         // Read R51 climate and settings state frames.
-        bool readFilter(const Frame& frame) const override {
+        bool readFilter(const Canny::Frame& frame) const override {
             return (frame.id() & 0xFFFFFFFE) == 0x54A ||
                    (frame.id() & 0xFFFFFFFE) == 0x72E ||
                     frame.id() == 0x625;
         }
 
         // Send R51 climate and settings control frames.
-        bool writeFilter(const Frame& frame) const override {
+        bool writeFilter(const Canny::Frame& frame) const override {
             return (frame.id() & 0xFFFFFFFE) == 0x540 ||
                    (frame.id() & 0xFFFFFFFE) == 0x71E;
         }
 
         // Log read errors to debug serial.
-        void onReadError(Error err) override {
+        void onReadError(Canny::Error err) override {
             ERROR_MSG_VAL("can: read error: ", err);
         }
 
         // Log write errors to debug serial.
-        void onWriteError(Error err, const Frame& frame) override {
+        void onWriteError(Canny::Error err, const Canny::Frame& frame) override {
             ERROR_MSG_VAL("can: write error: ", err);
             ERROR_MSG_VAL("can: dropped frame: ", frame);
         }
