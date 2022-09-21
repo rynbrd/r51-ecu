30adae09dab4bd6de1e53d3b2df9943d3365f0b3
Ryan Bourgeois <bluedragonx@gmail.com>
2022-09-21 11:22:34 -0700

    Add Scratch to Common.

--- libraries/Common/src/Common.h
+++ libraries/Common/src/Common.h
@@ -5,6 +5,7 @@
 #include "Common/CAN.h"
 #include "Common/Event.h"
 #include "Common/Message.h"
+#include "Common/Scratch.h"
 #include "Common/Ticker.h"
 
 #endif  // _R51_COMMON_H_
---
+++ libraries/Common/src/Common/Scratch.h
@@ -0,0 +1,23 @@
+#ifndef _R51_COMMON_SCRATCH_H_
+#define _R51_COMMON_SCRATCH_H_
+
+namespace R51 {
+
+static const size_t kScratchCapacity = 256;
+
+// A scratch space for temporary storage that's easily pre-allocated.
+struct Scratch {
+    // The byte buffer that holds the data in the space.
+    uint8_t bytes[kScratchCapacity];
+    // The number of bytes currently occupying the spacce.
+    size_t size;
+
+    // Create a new buffer.
+    Scratch() : size(0) {
+        memset(bytes, 0, kScratchCapacity);
+    }
+};
+
+}  // namespace R51
+
+#endif  // _R51_COMMON_SCRATCH_H_
