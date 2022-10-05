85362644ccaa42c9f8496db00ca276ab040f9a61
Ryan Bourgeois <bluedragonx@gmail.com>
2022-10-04 17:27:41 -0700

    Remove Core0 from bridge and clean up config.

--- bridge/Config.h
+++ bridge/Config.h
@@ -1,27 +1,26 @@
 #ifndef _R51_BRIDGE_CONFIG_H_
 #define _R51_BRIDGE_CONFIG_H_
 
+// Uncomment the following line to enable debug console.
+#define DEBUG_ENABLE
+
+// Serial configuration for the debug console.
 #define SERIAL_DEVICE Serial
 #define SERIAL_BAUDRATE 115200
 #define SERIAL_WAIT true
 
 // Vehicle CAN bus mode and speed. This is CAN 2.0 at 500K for the R51.
 #define VEHICLE_CAN_MODE Canny::CAN20_500K
-#define VEHICLE_READ_BUFFER 10
+#define VEHICLE_READ_BUFFER 16
 #define VEHICLE_WRITE_BUFFER 2
 
 // Uncomment to enable J1939 on boards that support it.
 #define J1939_ENABLE
 #define J1939_ADDRESS 0x19
+#define J1939_NAME 0x000013B000FFFAC0
 #define J1939_CAN_MODE Canny::CAN20_250K
-#define J1939_READ_BUFFER 4
+#define J1939_READ_BUFFER 32
 #define J1939_WRITE_BUFFER 2
-
-// Uncomment the following line to enable debug output.
-#define DEBUG_ENABLE
-
-// Uncomment to enable Console over serial.
-#define CONSOLE_ENABLE
 
 // Uncomment the following line to enable defog heater control.
 #define DEFOG_HEATER_ENABLE
@@ -39,8 +38,8 @@
 #define BLUETOOTH_SPI_IRQ_PIN 23
 #define BLUETOOTH_UPDATE_MS 1000
 
-// Uncomment to enable RealDash serial.
-#define REALDASH_ENABLE
+// RealDash configuration. RealDash uses the BLE connction and is enabled
+// automatically when BLE is configured above.
 #define REALDASH_FRAME_ID 0x10
 #define REALDASH_HB_ID 0x20
 #define REALDASH_HB_MS 500
--- bridge/bridge.ino
+++ bridge/bridge.ino
@@ -2,17 +2,109 @@
 #include "Debug.h"
 
 #include <Arduino.h>
-
+#include <Bluetooth.h>
 #include <Canny.h>
 #include <Canny/Detect.h>
+#include <Canny/RealDash.h>
+#include <Caster.h>
+#include <Console.h>
+#include <Vehicle.h>
 #include "CAN.h"
-#include "Core0.h"
+#include "Debug.h"
+#include "J1939.h"
+#include "Pico.h"
+#include "RealDash.h"
 
-#if defined(J1939_ENABLE) && defined(RASPBERRYPI_PICO)
-R51::Core0 core0(&CAN, &CAN1, J1939_ADDRESS);
-#else
-R51::Core0 core0(&CAN);
+using ::Caster::Bus;
+using ::Caster::Node;
+using ::R51::BLE;
+using ::R51::BLENode;
+using ::R51::CANNode;
+using ::R51::Climate;
+using ::R51::Defog;
+using ::R51::FilteredCAN;
+using ::R51::FilteredJ1939;
+using ::R51::IPDM;
+using ::R51::J1939Adapter;
+using ::R51::J1939AddressClaim;
+using ::R51::Message;
+using ::R51::PicoConfigStore;
+using ::R51::RealDashAdapter;
+using ::R51::Settings;
+using ::R51::SteeringKeypad;
+using ::R51::TirePressureState;
+
+// debug console
+#if defined(DEBUG_ENABLE)
+R51::ConsoleNode console(&SERIAL_DEVICE);
 #endif
+
+// vehicle CAN connection
+FilteredCAN can_conn(&CAN);
+CANNode<Canny::Frame> can_node(&can_conn);
+
+// control system J1939 connection
+#if defined(J1939_ENABLE)
+FilteredJ1939 j1939_conn(&CAN1, J1939_ADDRESS);
+J1939Adapter j1939_event_node(&j1939_conn);
+J1939AddressClaim j1939_claim_node(J1939_ADDRESS, J1939_NAME);
+#endif
+
+// RealDash over Bluetooth
+#if defined(BLUETOOTH_ENABLE)
+BLE ble_conn(BLUETOOTH_SPI_CS_PIN, BLUETOOTH_SPI_IRQ_PIN);
+BLENode ble_monitor(&ble_conn);
+
+Canny::RealDash realdash_serial(&ble_conn);
+RealDashAdapter realdash(&realdash_serial, REALDASH_FRAME_ID,
+        REALDASH_HB_ID, REALDASH_HB_MS);
+
+void onBluetoothConnect(void*) {
+    ble_monitor.onConnect();
+}
+
+void onBluetoothDisconnect(void*) {
+    ble_monitor.onDisconnect();
+}
+#endif
+
+// vehicle management
+PicoConfigStore config;
+Climate climate;
+Settings settings;
+IPDM ipdm;
+TirePressureState tires_node;
+#if defined(DEFOG_HEATER_ENABLE)
+Defog defog(DEFOG_HEATER_PIN, DEFOG_HEATER_MS);
+#endif
+#if defined(STEERING_KEYPAD_ENABLE)
+SteeringKeypad steering_keypad(STEERING_PIN_A, STEERING_PIN_B);
+#endif
+
+Node<Message>* nodes[] = {
+#if defined(DEBUG_ENABLE)
+    &console,
+#endif
+    &can_node,
+#if defined(J1939_ENABLE)
+    &j1939_event_node,
+    &j1939_claim_node,
+#endif
+#if defined(BLUETOOTH_ENABLE)
+    &ble_monitor,
+    &realdash,
+    &climate,
+    &settings,
+#endif
+    &ipdm,
+#if defined(DEFOG_HEATER_ENABLE)
+    &defog,
+#endif
+#if defined(STEERING_KEYPAD_ENABLE)
+    &steering_keypad,
+#endif
+};
+Bus<Message> bus(nodes, sizeof(nodes)/sizeof(nodes[0]));
 
 void setup_serial() {
     SERIAL_DEVICE.begin(SERIAL_BAUDRATE);
@@ -33,7 +125,7 @@ void setup_can() {
 }
 
 void setup_j1939() {
-#if defined(J1939_ENABLE) && defined(RASPBERRYPI_PICO)
+#if defined(J1939_ENABLE)
     DEBUG_MSG("setup: connecting to J1939");
     while (!CAN1.begin(J1939_CAN_MODE)) {
         DEBUG_MSG("setup: failed to connect to J1939");
@@ -42,15 +134,34 @@ void setup_j1939() {
 #endif
 }
 
+void setup_ble() {
+#if defined(BLUETOOTH_ENABLE)
+    DEBUG_MSG("setup: initializing bluetooth");
+    while (!ble_conn.begin()) {
+        DEBUG_MSG("setup: failed to init bluetooth");
+        delay(500);
+    }
+    ble_conn.setOnConnect(onBluetoothConnect, nullptr);
+    ble_conn.setOnDisconnect(onBluetoothDisconnect, nullptr);
+#endif
+}
+
 void setup() {
     setup_serial();
     setup_can();
     setup_j1939();
-
-    core0.setup();
+    setup_ble();
     DEBUG_MSG("setup: ECU running");
+    bus.init();
 }
 
 void loop() {
-    core0.loop();
+    bus.loop();
+#if defined(BLUETOOTH_ENABLE)
+    // BLE's update method needs to be called to enable connectivity callbacks
+    ble_conn.update(BLUETOOTH_UPDATE_MS);
+#endif
+#if defined(DEBUG_ENABLE)
+    delay(10);
+#endif
 }
