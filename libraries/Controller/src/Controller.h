9d2a45e58a0a6a393eb747203f2bbbce304e8c51
Ryan Bourgeois <bluedragonx@gmail.com>
2022-10-14 11:24:30 -0700

    Add missing controller includes.

--- libraries/Controller/src/Controller.h
+++ libraries/Controller/src/Controller.h
@@ -4,6 +4,8 @@
 #include "Controller/Audio.h"
 #include "Controller/Fusion.h"
 #include "Controller/HMI.h"
+#include "Controller/HMIEvent.h"
+#include "Controller/Nav.h"
 #include "Controller/Steering.h"
 
 #endif  // _R51_CONTROLLER_H_
