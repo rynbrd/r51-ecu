87784237966c5bd0c273e1fbe1f1be19824a2c96
Ryan Bourgeois <bluedragonx@gmail.com>
2022-10-16 18:41:53 -0700

    Fix printing of unsigned 64-bit integers. Some Arduino hardware packages lack the appropriate print overloads.

--- libraries/Common/src/Common/J1939Claim.cpp
+++ libraries/Common/src/Common/J1939Claim.cpp
@@ -1,11 +1,36 @@
 #include "J1939Claim.h"
 
 #include <Arduino.h>
+#include <Endian.h>
 
 namespace R51 {
+namespace {
+
+void printHexByte(Print& p, uint8_t byte) {
+    if (byte <= 0x0F)  {
+        p.print('0');
+    }
+    p.print(byte, HEX);
+}
+
+size_t printHexUint64(Print& p, uint64_t value) {
+    uint8_t* bytes = (uint8_t*)&value;
+    if (Endian::host() ==  Endian::BIG) {
+        for (uint8_t i = 0; i < 8; ++i) {
+            printHexByte(p, bytes[i]);
+        }
+    } else {
+        for (uint8_t i = 8; i > 0; --i) {
+            printHexByte(p, bytes[i - 1]);
+        }
+    }
+    return 16;
+}
+
+}  // namespace
 
 size_t J1939Claim::printTo(Print& p) const {
-    return p.print(address_, HEX) + p.print(":") + p.print(name_, HEX);
+    return p.print(address_, HEX) + p.print(":") + printHexUint64(p, name_);
 }
 
 bool operator==(const J1939Claim& left, const J1939Claim& right) {
