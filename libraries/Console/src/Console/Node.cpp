bee98da2de9e75b21fd9b0cfe14718b858f46ae0
Ryan Bourgeois <bluedragonx@gmail.com>
2023-01-08 18:32:20 -0800

    Console pretty-prints climate state.

--- libraries/Console/src/Console/Node.cpp
+++ libraries/Console/src/Console/Node.cpp
@@ -3,9 +3,76 @@
 #include <Canny.h>
 #include <Caster.h>
 #include <Core.h>
+#include <Vehicle.h>
 #include "Console.h"
 
+#define isEvent(e, s, i) (e->subsystem == (uint8_t)s && e->id == (uint8_t)i)
+
 namespace R51 {
+namespace {
+
+void prettyPrintEvent(Stream* stream, const Event* event) {
+    if (isEvent(event, SubSystem::CLIMATE, ClimateEvent::SYSTEM_STATE)) {
+        const auto* s = (ClimateSystemState*)event;
+        stream->print(" (climate ");
+        switch (s->mode()) {
+            case CLIMATE_SYSTEM_OFF:
+                stream->print("off");
+                break;
+            case CLIMATE_SYSTEM_AUTO:
+                stream->print("auto");
+                break;
+            case CLIMATE_SYSTEM_MANUAL:
+                stream->print("manual");
+                break;
+            case CLIMATE_SYSTEM_DEFOG:
+                stream->print("defog");
+                break;
+            default:
+                break;
+        }
+        stream->print(", ac ");
+        stream->print(s->ac() ? "on" : "off");
+        stream->print(", dual ");
+        stream->print(s->dual() ? "on" : "off");
+        stream->print(")");
+    } else if (isEvent(event, SubSystem::CLIMATE, ClimateEvent::AIRFLOW_STATE)) {
+        const auto* s = (ClimateAirflowState*)event;
+        stream->print(" (climate fan ");
+        stream->print(s->fan_speed());
+        if (s->face()) {
+            stream->print(" face");
+        }
+        if (s->feet()) {
+            stream->print(" feet");
+        }
+        if (s->windshield()) { 
+            stream->print(" windshield");
+        }
+        if (s->recirculate()) {
+            stream->print(" recirculate");
+        }
+        stream->print(")");
+    } else if (isEvent(event, SubSystem::CLIMATE, ClimateEvent::TEMP_STATE)) {
+        const auto* s = (ClimateTempState*)event;
+        char unit = 'F';
+        if (s->units() == UNITS_METRIC) {
+            unit = 'C';
+        }
+        stream->print(" (climate driver temp ");
+        stream->print(s->driver_temp());
+        stream->print(unit);
+        stream->print(", passenger temp ");
+        stream->print(s->passenger_temp());
+        stream->print(unit);
+        stream->print(", outside temp ");
+        stream->print(s->outside_temp());
+        stream->print(unit);
+        stream->print(")");
+    }
+}
+
+}  // namespace
 
 void ConsoleNode::emit(const Caster::Yield<Message>& yield) {
     Reader::Error err;
@@ -39,6 +106,7 @@ void ConsoleNode::handle(const Message& msg, const Caster::Yield<R51::Message>&)
             if (!console_.event_mute()) {
                 console_.stream()->print("console: event recv ");
                 msg.event()->printTo(*console_.stream());
+                prettyPrintEvent(console_.stream(), msg.event());
                 console_.stream()->println();
             }
             break;
